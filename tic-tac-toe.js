c59b67fea8492f24915c6e5e5f7ab5ee474b00cf
Deirdre Storck <deirdre.storck@gmail.com>
2016-06-30 14:58:48 -0700

    added logic to find a winner

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -23,34 +23,78 @@ function TicTacToe() {
       this.board[space] = mark
     }
     this.turn += 1
-  }
-}
+    console.log(this.is_a_winner())
+    if (this.is_a_winner()) {
+      console.log('yahoooo')
 
+    }
+  }
+
+  this.reset = function() {
+    this.turn = 1
+    for (var element in this.board) {
+      this.board[element] = null
+    }
+  }
+
+  this.is_a_winner = function() {
+
+    if (this.board['one_a'] === this.board['one_b'] && this.board['one_b'] === this.board['one_c'] && this.board['one_a'] != null) {
+      return true
+    }
+    else if (this.board['two_a'] === this.board['two_b'] && this.board['two_b'] === this.board['two_c'] && this.board['two_a'] != null) {
+      return true
+    } else if (this.board['three_a'] === this.board['three_b'] && this.board['three_b'] === this.board['three_c'] && this.board['three_a'] != null) {
+      return true
+    } else if (this.board['one_a'] === this.board['two_a'] && this.board['two_a'] === this.board['three_a'] && this.board['one_a'] != null) {
+        return true
+    } else if (this.board['one_b'] === this.board['two_b'] && this.board['two_b'] === this.board['three_b'] && this.board['one_b'] != null) {
+        return true
+    } else if (this.board['one_c'] === this.board['two_c'] && this.board['two_c'] === this.board['three_c'] && this.board['three_c'] != null) {
+        return true
+    } else if (this.board['one_a'] === this.board['two_b'] && this.board['two_b'] === this.board['three_c'] && this.board['one_a'] != null) {
+        return true
+    } else if (this.board['one_c'] === this.board['two_b'] && this.board['two_b'] === this.board['three_a'] && this.board['two_b'] != null) {
+        return true
+    } else {
+      return false
+    }
+  }
+
+}
 
 
 $(document).ready(function() {
  var container = $('#tic-tac-toe')
- var buttons = container.children().children().children().children().children()
- // var display = container.children().children('.display')
- console.log(buttons)
- // var note_id = $(this).data('note') + 'Audio'
- //  var note = $('#' + note_id)[0]
-
+ // var buttons = container.children().children().children().children().children()
+ var buttons = $('.space')
+ var reset_button = container.children('.reset')
  var game = new TicTacToe
+
  buttons.on('click', function(event) {
    event.preventDefault()
    var button = $(this)
    var space_id = $(this).data('space')
+
   //  console.log("space id : ",space_id)
+  console.log('game board')
+  console.log(game.board)
 
    if (button.hasClass('space')) {
      console.log('you tried to push' , button)
      game.mark(space_id)
-     var display = container.children().children().children().children().children('button.' + space_id)
+     var display = container.find('button.' + space_id)
+    //  var display = container.children().children().children().children().children('button.' + space_id)
      display.text(game.board[space_id])
-     console.log(game.board)
-     console.log(game.turn)
    }
+ })
+
+ reset_button.on('click', function(event) {
+
+   event.preventDefault()
+  //  var button = $(this)
+   game.reset()
+   $(".space").text("")
 
  })
 
