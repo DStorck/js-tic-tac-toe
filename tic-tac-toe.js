b5f020bd53ccd962355be9292b122ea8d71bccda
Deirdre Storck <deirdre.storck@gmail.com>
2016-06-30 13:28:07 -0700

    got some basic functions working

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,57 @@
 function TicTacToe() {
+  this.turn = 1
+  this.board = {
+  "one_a": null,
+  "one_b": null,
+  "one_c": null,
+  "two_a": null,
+  "two_b": null,
+  "two_c": null,
+  "three_a": null,
+  "three_b": null,
+  "three_c": null
+};
 
+
+  this.mark = function(space) {
+    if ( this.turn % 2 === 0 ) {
+      var mark = "A"
+    } else {
+      var mark = "B"
+    }
+    if (this.board[space] === null) {
+      this.board[space] = mark
+    }
+    this.turn += 1
+  }
 }
 
-TicTacToe.prototype = {
-  
-}
+
+
+$(document).ready(function() {
+ var container = $('#tic-tac-toe')
+ var buttons = container.children().children()
+ var display = container.children('.display')
+ console.log(buttons)
+ // var note_id = $(this).data('note') + 'Audio'
+ //  var note = $('#' + note_id)[0]
+
+ var game = new TicTacToe
+ buttons.on('click', function(event) {
+   event.preventDefault()
+   var button = $(this)
+   var space_id = $(this).data('space')
+  //  console.log("space id : ",space_id)
+
+   if (button.hasClass('space')) {
+     console.log('you tried to push' , button)
+     game.mark(space_id)
+     var display = container.children().children('button.' + space_id)
+     display.text(game.board[space_id])
+     console.log(game.board)
+     console.log(game.turn)
+   }
+
+ })
+
+})
