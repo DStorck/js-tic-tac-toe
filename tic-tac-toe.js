e3e221833a6388f98215dd8752e9d14c0393a57d
Deirdre Storck <deirdre.storck@gmail.com>
2016-07-01 12:44:02 -0700

    added coin for scoring and score updated as game is played

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,5 +1,7 @@
 function TicTacToe() {
-  this.turn = 1
+  this.turn = 2
+  this.mario_wins = 0
+  this.goomba_wins = 0
   this.board = {
   "one_a": null,
   "one_b": null,
@@ -12,6 +14,21 @@ function TicTacToe() {
   "three_c": null
 };
 
+  this.animate_player = function() {
+    var $mario = $('div#mario img.mario')
+    var $goomba = $('div#bad_guy img.goomba')
+    var mario_on = "media/mario-runs.gif"
+    var mario_off= "media/mario-still.png"
+    var goomba_on= "media/goombagif.gif"
+    var goomba_off= "media/goomba-still.png"
+    if (this.turn % 2 === 0) {
+      $mario.attr('src', mario_on)
+      $goomba.attr('src', goomba_off)
+    } else {
+      $mario.attr('src', mario_off)
+      $goomba.attr('src', goomba_on)
+    }
+  }
 
   this.mark = function(space) {
     if ( this.turn % 2 === 0 ) {
@@ -21,11 +38,8 @@ function TicTacToe() {
     }
     if (this.board[space] === null) {
       this.board[space] = mark
-    }
-    this.turn += 1
-    console.log(this.is_a_winner())
-    if (this.is_a_winner()) {
-      console.log('yahoooo')
+      this.turn += 1
+      this.animate_player()
     }
   }
 
@@ -45,23 +59,64 @@ function TicTacToe() {
     return true
   }
 
+
   this.is_a_winner = function() {
     if (this.board['one_a'] === this.board['one_b'] && this.board['one_b'] === this.board['one_c'] && this.board['one_a'] != null) {
+      if (this.board['one_a'] === "A") {
+        this.mario_wins += 1
+      } else {
+        this.goomba_wins += 1
+      }
       return true
     }
     else if (this.board['two_a'] === this.board['two_b'] && this.board['two_b'] === this.board['two_c'] && this.board['two_a'] != null) {
+      if (this.board['two_a'] === "A") {
+        this.mario_wins += 1
+      } else {
+        this.goomba_wins += 1
+      }
       return true
     } else if (this.board['three_a'] === this.board['three_b'] && this.board['three_b'] === this.board['three_c'] && this.board['three_a'] != null) {
+      if (this.board['three_a'] === "A") {
+        this.mario_wins += 1
+      } else {
+        this.goomba_wins += 1
+      }
       return true
     } else if (this.board['one_a'] === this.board['two_a'] && this.board['two_a'] === this.board['three_a'] && this.board['one_a'] != null) {
+      if (this.board['one_a'] === "A") {
+        this.mario_wins += 1
+      } else {
+        this.goomba_wins += 1
+      }
         return true
     } else if (this.board['one_b'] === this.board['two_b'] && this.board['two_b'] === this.board['three_b'] && this.board['one_b'] != null) {
+      if (this.board['one_b'] === "A") {
+        this.mario_wins += 1
+      } else {
+        this.goomba_wins += 1
+      }
         return true
     } else if (this.board['one_c'] === this.board['two_c'] && this.board['two_c'] === this.board['three_c'] && this.board['three_c'] != null) {
+      if (this.board['one_c'] === "A") {
+        this.mario_wins += 1
+      } else {
+        this.goomba_wins += 1
+      }
         return true
     } else if (this.board['one_a'] === this.board['two_b'] && this.board['two_b'] === this.board['three_c'] && this.board['one_a'] != null) {
+      if (this.board['one_a'] === "A") {
+        this.mario_wins += 1
+      } else {
+        this.goomba_wins += 1
+      }
         return true
     } else if (this.board['one_c'] === this.board['two_b'] && this.board['two_b'] === this.board['three_a'] && this.board['two_b'] != null) {
+      if (this.board['one_c'] === "A") {
+        this.mario_wins += 1
+      } else {
+        this.goomba_wins += 1
+      }
         return true
     } else {
       return false
@@ -77,39 +132,38 @@ $(document).ready(function() {
  var buttons = $('.space')
  var reset_button = container.children('.reset')
  var game = new TicTacToe
+ game.animate_player()
 
  buttons.on('click', function(event) {
    event.preventDefault()
    var button = $(this)
    var space_id = $(this).data('space')
 
-   console.log('game board')
-   console.log(game.board)
+  if (button.hasClass('space')) {
+     $('#coin')[0].play();
+     game.mark(space_id)
 
-    if (button.hasClass('space')) {
-       console.log('you tried to push' , button)
-       $('#coin')[0].play();
-       game.mark(space_id)
-       console.log(game.is_a_winner())
-       console.log(game.is_a_draw())
-       if (game.is_a_winner()) {
-        $('#winner')[0].play();
-    }
+     if (game.is_a_winner()) {
+      $('#winner')[0].play();
+      $mario_score =  $('div#mario span#mario-score')
+      $goomba_score =  $('div#bad_guy span#goomba-score')
+      $mario_score.text(game.mario_wins)
+      $goomba_score.text(game.goomba_wins)
+     }
+  }
 
-    if (game.is_a_draw()) {
-      $('#tie')[0].play();
-    }
+  if (game.is_a_draw()) {
+    $('#tie')[0].play();
+  }
 
-    var display = container.find('button.' + space_id)
-    var image = display.find('img')
-    if (game.board[space_id] === "A") {
-      image.attr("src","media/mario.png");
-    } else if (game.board[space_id] === "B") {
-      image.attr("src","media/bad_guy.png");
+  var display = container.find('button.' + space_id)
+  var image = display.find('img')
+  if (game.board[space_id] === "A") {
+    image.attr("src","media/mario.png");
+  } else if (game.board[space_id] === "B") {
+    image.attr("src","media/bad_guy.png");
+  }
 
-    }
-    //  display.text(game.board[space_id])
-   }
  })
 
  reset_button.on('click', function(event) {
@@ -121,5 +175,7 @@ $(document).ready(function() {
    image.attr('src', 'media/empty.png')
 
  })
+
+
 
 })
